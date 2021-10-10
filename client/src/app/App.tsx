6997648331b215c153ee573e40926a6cf87c7821
Craig <indigomx9@gmail.com>
2021-10-10 19:51:21 -0400

    Initial setup of the React.

---
+++ client/src/app/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+export const App = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis deserunt quibusdam harum quam repudiandae optio ex quos. Magnam temporibus sit accusamus numquam a hic totam quod aliquam quis aspernatur! Est!</p>
+        </React.Fragment>
+    );
+};
+
+
+
+
+
---
+++ client/src/components/Main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Main = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>Main</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates nemo amet accusantium saepe consequatur impedit tempore a asperiores deserunt! Nulla quia maiores neque amet, voluptas tenetur delectus cupiditate reprehenderit similique.</p>
+        </React.Fragment>
+    );
+};
+
+
+
+
+
